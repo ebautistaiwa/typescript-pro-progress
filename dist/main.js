2311348e757409a27b4c8cdc1c65d43b45c2067e
alzuhara <ebautista@iwa.com.mx>
2017-09-30 20:26:08 -0500

    Crear archivos de definición

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+/** @internal */
+function main() {
+    console.log('TypeScript Pro!');
+}
+function foo() { }
+main();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+/** @internal */
 function main(){
     console.log('TypeScript Pro!')
 }
+
+function foo(){}
 
 main()
