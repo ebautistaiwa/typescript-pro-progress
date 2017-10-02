323de092a4adaf55722c8582d100b8bed2d059b9
Emanuel <ebautista@iwa.com.mx>
2017-10-02 13:00:16 -0500

    Afinando la adquisición de tipos

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,2 @@
-//import polyfill
-import 'reflect-metadata'
-import { Component, Input } from '@angular/core'
-
-/** 
- * Configure Component
-*/
-@Component({
-    selector: 'app'
-})
-class AppComponent{
-    @Input() foo: string
-}
+foo
+$
