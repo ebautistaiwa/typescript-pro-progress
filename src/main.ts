0e18379b9f47b21b230d4d59506a8d54a7792b52
Emanuel <ebautista@iwa.com.mx>
2017-10-05 17:17:52 -0500

    validar tipos de datos propios y customizados

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
-'use strict'
-
-interface PetStore{
-    pets: string[],
-    createRandomPetPicker(): Function
+interface Bar{
+    bar: any
 }
 
-const petStore : PetStore = {
-    pets: ['dog', 'cat'],
-    createRandomPetPicker: function(this: PetStore){
-        return () => {
-            const pets = this.pets
-            const i = Math.floor(Math.random() * pets.length)
-            return pets[i]
-        }
+function isBar(arg: any): arg is Bar{
+    return arg.bar !== 'undefined'
+}
+
+function isString(arg: any): arg is string{
+    return typeof arg === 'string'
+}
+
+function foo(x: number | string){
+    if(isString(x)){
+        x.substr
     }
 }
-
-const getRandomPet = petStore.createRandomPetPicker()
-console.log(getRandomPet())
